125ad42b86162e8c966d2c129cedc331ff569f3d
agus <139150745+Rayzeng45@users.noreply.github.com>
2023-11-28 19:30:50 -0300

    Agregué Mecanicas y una escena mas prefaps
    Barra de vida, Rework de damage spike

--- Assets/Scrips/DamageSpike.cs
+++ Assets/Scrips/DamageSpike.cs
@@ -1,19 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageSpike : MonoBehaviour
-{
-
-    private void OnCollisionEnter2D(Collision2D collision)
+{    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             Debug.Log("Player Damaged");
-            collision.transform.GetComponent<PlayerRespawn>().PlayerDamaged();
-        }   
+            collision.transform.GetComponent<Player>().PlayerDamaged(10);
+        }
     }
-
-
-
 }
